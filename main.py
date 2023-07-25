6a94f30fefa822f80129bc9297edf41e47bccba5
Imanm02 <imanm1381@gmail.com>
2023-07-25 18:52:56 +0330

    add comments to script

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 import json
 import pandas as pd
 from openpyxl.styles import Alignment, Font, PatternFill
@@ -5,8 +6,10 @@ from openpyxl.utils import get_column_letter
 import openpyxl
 from openpyxl.styles import numbers
 
+# Define the JSON file names
 files = ["1010.json", "1014.json", "2010.json", "2011.json", "3010.json", "3012.json"]
 
+# Define a mapping from JSON file names to sheet names
 sheet_names = {
     "1010.json": "برنامه‌سازی پیشرفته",
     "1014.json": "داده‌ساختارها و الگوریتم‌ها",
@@ -16,16 +19,21 @@ sheet_names = {
     "3012.json": "ریاضیات هوش مصنوعی",
 }
 
+# Create a Pandas Excel writer using openpyxl as the engine
 writer = pd.ExcelWriter('output.xlsx', engine='openpyxl')
 
+# Iterate over each JSON file
 for file in files:
+    # Open and load the JSON file
     with open(file) as f:
         json_data = json.load(f)
 
+    # Extract data from JSON
     course_name = json_data["courses"][0]["name"]
     course_code = json_data["courses"][0]["code"]
     students = json_data["courses"][0]["current_group"][0]["students"]
 
+    # Prepare the data for the DataFrame
     data = {
         "Course Name": [course_name] * len(students),
         "Course Code": [course_code] * len(students),
@@ -40,36 +48,48 @@ for file in files:
         "Status": [student["pivot"]["status"] for student in students],
     }
 
+    # Convert the data into a Pandas DataFrame
     df = pd.DataFrame(data)
 
+    # Replace values in 'Status' column as per requirements
     df['Status'] = df['Status'].replace({'pending': 'ثبت‌نام', 'requesting': 'کردیت'})
 
+    # Sort DataFrame by 'Status' column
     df = df.sort_values('Status')
 
+    # Write the DataFrame to an Excel file
     df.to_excel(writer, sheet_name=sheet_names[file], index=False)  
 
+# Save the changes in writer object
 writer.save()
 
+# Define Excel formatting variables
 font = Font(name='Vazirmatn')
 header_fill = PatternFill(start_color="FFFF99", end_color="FFFF99", fill_type="solid")
 other_fill = PatternFill(start_color="CCFFCC", end_color="CCFFCC", fill_type="solid")
 
+# Define column widths
 column_widths = {"Email": 30, "Student ID": 10, "Course Code": 10, "Status": 10, "Gender": 10, "Name": 15, "Surname": 15}
 
+# Load the workbook
 book = openpyxl.load_workbook('output.xlsx')
 
+# Iterate over each sheet in the workbook
 for sheet in book.sheetnames:
     worksheet = book[sheet]
+    # Iterate over each column in DataFrame and set column width and cell format
     for i, column in enumerate(df.columns, start=1):
         if column in column_widths:
             worksheet.column_dimensions[get_column_letter(i)].width = column_widths[column]
         else:
             worksheet.column_dimensions[get_column_letter(i)].width = 20
 
+        # if the column contains numbers, set the cell format to 'Text'
         if column in ["Student ID", "Mobile Number", "National Code", "Phone Number", "Status"]:
             for cell in worksheet[get_column_letter(i)]:
                 cell.number_format = numbers.FORMAT_TEXT
 
+    # Iterate over each cell in the worksheet and set alignment, font, and fill
     for row in worksheet.iter_rows():
         for cell in row:
             cell.alignment = Alignment(horizontal='center', vertical='center')
@@ -79,4 +99,5 @@ for sheet in book.sheetnames:
             else:
                 cell.fill = other_fill
 
+# Save the changes in the workbook
 book.save('output.xlsx')
